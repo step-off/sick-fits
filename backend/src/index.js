b48feda4afe6f871cc1b5665f8e94f92138feca6
step-off <stepanoff@skbkontur.ru>
2019-12-31 09:31:11 +0500

    chore(backend): setup graphql server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-// let's go!
+require('dotenv').config();
+const createServer = require('./createServer');
+
+const server = createServer();
+
+server.start({
+	cors: {
+		credentials: true,
+		origin: process.env.FRONTEND_URL
+	}
+}, options => {
+	console.log(`Server is running on port: ${options.port}`)
+})
