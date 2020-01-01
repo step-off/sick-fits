8b2da3a7ecccfff8bdd8c2d75751adaaddd0c71a
step-off <nofantasy12@gmail.com>
2020-01-01 15:00:19 +0500

    feat(frontend): setup apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,28 @@
 import App from 'next/app';
+import {ApolloProvider} from 'react-apollo'
 import Page from "./Page";
+import withData from "../lib/withData";
 
 export class MyApp extends App {
+	static async getInitialProps({Component, ctx}) {
+		let pageProps = {}
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		
+		pageProps.query = ctx.query;
+		return {pageProps}
+	}
+	
 	render() {
-		const {Component, ...rest} = this.props;
+		const {Component, apollo, ...rest} = this.props;
 
-		return <React.Fragment>
+		return <ApolloProvider client={apollo}>
 			<Page>
 				<Component {...rest}/>
 			</Page>
-		</React.Fragment>
+		</ApolloProvider>
 	}
 }
 
-export default MyApp;
+export default withData(MyApp);
