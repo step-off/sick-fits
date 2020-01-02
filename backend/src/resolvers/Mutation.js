70b33578f992ef2449d8bc22c56d2c9b04cea50d
step-off <nofantasy12@gmail.com>
2020-01-02 14:50:31 +0500

    feat(Item): can delete item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -13,7 +13,12 @@ const mutations = {
 				id
 			}
 		}, info)
-	}
+	},
+	async deleteItem(parent, args, ctx, info) {
+		const where = {id: args.id};
+		const item = await ctx.db.query.item({where}, `{ id title}`);
+		return ctx.db.mutation.deleteItem({where}, info);
+	},
 };
 
 module.exports = mutations;
---
+++ frontend/components/DeleteItem.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {Mutation} from 'react-apollo';
+import gql from 'graphql-tag';
+import {ALL_ITEMS_QUERY} from './Items';
+import {ApolloConsumer} from 'react-apollo'
+
+const DELETE_ITEM_MUTATION = gql`
+    mutation DELETE_ITEM_MUTATION($id: ID!) {
+        deleteItem(id: $id) {
+            id
+        }
+    }
+`;
+
+class DeleteItem extends Component {
+	render() {
+		return (
+			<ApolloConsumer>
+				{client => (
+					<Mutation
+						mutation={DELETE_ITEM_MUTATION}
+						variables={{id: this.props.id}}
+						update={this.update.bind(this, client)}
+					>
+						{(deleteItem, {error}) => (
+							<button
+								onClick={() => this.handleDeletion(deleteItem)}
+							>
+								{this.props.children}
+							</button>
+						)}
+					</Mutation>
+				)}
+			</ApolloConsumer>
+		);
+	}
+
+	update = (client, cache, payload) => {
+		const dataInCache = client.readQuery({query: ALL_ITEMS_QUERY});
+		const data = {...dataInCache};
+		data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+		client.writeQuery({query: ALL_ITEMS_QUERY, data});
+	};
+
+	handleDeletion = (deleteItem) => {
+		if (confirm('Are you sure you want to delete this item?')) {
+			deleteItem();
+		}
+	};
+}
+
+export default DeleteItem;
