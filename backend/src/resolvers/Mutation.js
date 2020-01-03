4e660dd55b64b4b4e4bcf10f7679a1a759b2a543
step-off <nofantasy12@gmail.com>
2020-01-03 15:21:58 +0500

    feat(backend): add password reset flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {randomBytes} = require('crypto');
+const {promisify} = require('util');
 
 const mutations = {
 	async createItem(parent, args, ctx, info) {
@@ -38,7 +40,7 @@ const mutations = {
 			maxAge: 1000 * 3600 * 24 * 365,
 		});
 		return user;
-	}, 
+	},
 	async signin(parent, args, ctx, info) {
 		const email = args.email.toLowerCase();
 		const password = args.password;
@@ -64,7 +66,50 @@ const mutations = {
 		return {
 			message: 'Successfully signed out!'
 		}
-	}
+	},
+	async requestPasswordReset(parent, args, ctx, info) {
+		const user = await ctx.db.query.user({where: {email: args.email}});
+		if (!user) {
+			throw new Error(`No such user found for email ${args.email}`);
+		}
+		const randomBytesPromiseified = promisify(randomBytes);
+		const resetToken = (await randomBytesPromiseified(20)).toString('hex');
+		const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+		const res = await ctx.db.mutation.updateUser({
+			where: {email: args.email},
+			data: {resetToken, resetTokenExpiry},
+		});
+		return {message: ''};
+	},
+	async resetPassword(parent, args, ctx, info) {
+		if (args.password !== args.confirmPassword) {
+			throw new Error("Yo Passwords don't match!");
+		}
+		const [user] = await ctx.db.query.users({
+			where: {
+				resetToken: args.resetToken,
+				resetTokenExpiry_gte: Date.now() - 3600000,
+			},
+		});
+		if (!user) {
+			throw new Error('This token is either invalid or expired!');
+		}
+		const password = await bcrypt.hash(args.password, 10);
+		const updatedUser = await ctx.db.mutation.updateUser({
+			where: {email: user.email},
+			data: {
+				password,
+				resetToken: null,
+				resetTokenExpiry: null,
+			},
+		});
+		const token = jwt.sign({userId: updatedUser.id}, process.env.APP_SECRET);
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365,
+		});
+		return updatedUser;
+	},
 };
 
 module.exports = mutations;
